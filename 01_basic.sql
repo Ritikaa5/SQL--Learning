513959e88e78cc955cd1b80e9b2f434cc34bd40c
Ritikaa5 <ritikarajput472@gmail.com>
2026-08-15 12:45:06 +0530

    Add students insert queries

--- 01_basic.sql
+++ 01_basic.sql
@@ -2,8 +2,17 @@ CREATE DATABASE practice_db;
 
 USE practice_db;
 
-CREATE TABLE student(
+CREATE TABLE students(
     id INT,
     name VARCHAR(50),
     age INT
 );
+
+INSERT INTO students(id,name,age)
+VALUES(1,'Ritika',21);
+
+INSERT INTO student(id,name,age)
+VALUES(2,"Aman",22);
+
+INSERT INTO student(id,name,age)
+VALUES(3,"Priya",20);
