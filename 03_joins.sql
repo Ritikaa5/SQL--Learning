b67efe823fb0d460c4eec48a84586b962e303167
Ritikaa5 <ritikarajput472@gmail.com>
2026-08-18 21:50:38 +0530

    Add courses table and data

---
+++ 03_joins.sql
@@ -0,0 +1,13 @@
+-- Create course table
+CREATE TABLE course(
+    course_id INT,
+    course_name VARCHAR(20),
+    stu_id INT
+);
+
+-- INSERT COURSE DATA
+ INSERT INTO courses(course_id,course_name, stu_id)
+ VALUES
+ (101,'SQL',1),
+ (102,'JAVA',2),
+ (103,'Python',4); 
