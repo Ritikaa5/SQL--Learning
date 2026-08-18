668392f60a98a18e30e5b553fecac2712de0a2c1
Ritikaa5 <ritikarajput472@gmail.com>
2026-08-18 10:08:26 +0530

    Add GROUP BY and HAVING

---
+++ 02_group_by_having.sql
@@ -0,0 +1,12 @@
+-- GROUP BY: Count students for each age
+SELECT age,COUNT(*) AS total_students
+FROM students
+GROUP BY age;
+
+
+-- Having 
+-- Having: show age group with more than 1 student
+SELECT age, COUNT(*) AS total_stud
+FROM students
+GROUP BY age
+HAVING COUNT(*)>1;
