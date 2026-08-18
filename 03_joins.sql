49c9c49ac3d44befad2090a9b4a069c45d588449
Ritikaa5 <ritikarajput472@gmail.com>
2026-08-18 22:01:25 +0530

    Add INNER JOIN query

--- 03_joins.sql
+++ 03_joins.sql
@@ -11,3 +11,11 @@ CREATE TABLE course(
  (101,'SQL',1),
  (102,'JAVA',2),
  (103,'Python',4); 
+
+ --INNER JOIN
+ -- shows students with their courses
+ SELECT student.name,
+ course.course_name
+ FROM students
+ INNER JOIN courses
+ ON students.id = course.student_id;
